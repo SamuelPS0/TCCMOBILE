67004dc60db060c55d656199226241f6047e8157
SamuelPS0 <rm93980@estudante.fieb.edu.br>
2026-06-24 09:40:48 -0300

    branch

--- assets/api/globalapi.js
+++ assets/api/globalapi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //"http://10.0.2.2:8080/api/v1/"
 //"http://10.0.2.15:8080/api/v1/"
 //"http://localhost:8080/api/v1/"
-//"http://192.168.3.7:8080/api/v1/"
+//"http://192.168.3.7:8080/api/v1/"""
 
 const globalapi = axios.create({
   baseURL: "http://localhost:8080/api/v1/",
